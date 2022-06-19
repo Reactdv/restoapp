ee13e706ddc36bd381545dff5b07fa4edcd4cb94
Reactdv <jerllylara12@gmail.com>
2022-06-19 12:07:19 +0800

    header content

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 const App = () => (
   <div>
     <NavBar />
-   {/* <Header />
-    <AboutUs />
+    <Header />
+  {/*  <AboutUs />
     <SpecialMenu />
     <Chef />
     <Intro />
