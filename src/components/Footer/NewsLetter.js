8ba7f3450a300d6d38474684f28396f775f9f883
Reactdv <jerllylara12@gmail.com>
2022-06-19 09:32:51 +0800

    done navbar

---
+++ src/components/Footer/NewsLetter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './NewsLetter.css';
+
+const Newsletter = () => (
+  <div>
+    Newsletter
+  </div>
+);
+
+export default Newsletter;
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import SubHeading from './SubHeading/SubHeading';
+import NewsLetter from './Footer/NewsLetter';
+import FooterOverlay from './Footer/FooterOverlay';
+import MenuItem from './MenuItem/MenuItem';
+import NavBar from './NavBar/NavBar';
+
+export {
+  SubHeading,
+  NewsLetter,
+  FooterOverlay,
+  MenuItem,
+  NavBar,
+};
