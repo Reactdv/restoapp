6b09e05c44a2e8f7bcb2aaa2c842610b7f686f33
Reactdv <jerllylara12@gmail.com>
2022-06-20 17:46:58 +0800

    laurels

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,5 +1,7 @@
 import { images } from './images';
 
+const { award02,award01,award03,award05} = images
+
 const wines = [
   {
     title: 'Chapel Hill Shiraz',
@@ -58,22 +60,22 @@ const cocktails = [
 
 const awards = [
   {
-    imgUrl: images.award02,
+    imgUrl: award02,
     title: 'Bib Gourmond',
     subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
   },
   {
-    imgUrl: images.award01,
+    imgUrl: award01,
     title: 'Rising Star',
     subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
   },
   {
-    imgUrl: images.award05,
+    imgUrl: award05,
     title: 'AA Hospitality',
     subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
   },
   {
-    imgUrl: images.award03,
+    imgUrl: award03,
     title: 'Outstanding Chef',
     subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
   },
--- src/container/Laurels/Laurels.js
+++ src/container/Laurels/Laurels.js
@@ -2,10 +2,55 @@ import React from 'react';
 
 import './Laurels.css';
 
+import  { SubHeading }   from "../../components"
+
+import { images,data } from "../../constants/index.js"
+ 
+const { awards } = data
+
+
+
 const Laurels = () => (
-  <div>
-    Laurels
-  </div>
+   <div className="app__bg app__wrapper section__padding" id="awards">
+    
+     <div className="app__wrapper_info">
+        <SubHeading title="Awards & Recognition"/>
+        <h1 className="headtext__cormorant">
+         Our Laurels
+        </h1>
+        <div 
+        
+        className="app__laurels_awards">
+         
+       {awards.map((award,index)=>(
+         
+        <div 
+        className="app__laurels_awards-card"
+        key={index}>
+           <img src={award.imgUrl} />
+           
+           <div className="app__laurels_awards-card_content">
+             <p 
+             style={{color:"#dcca87"}}
+             className="p__cormorant">
+             {award.title}
+             </p>
+             <p 
+             className="p__cormorant">
+             {award.subtitle}
+             </p>
+           </div>
+        </div>
+         )
+       )}
+       
+        </div>  
+           
+     </div>
+     <div className="app__wrapper_img">
+        <img src={images.laurels}alt="laurels"/>
+     </div>
+   </div>
 );
 
 export default Laurels;
