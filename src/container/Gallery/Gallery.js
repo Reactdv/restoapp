d7a1f4c88a4a870755e1ea31d187ef885a12b848
Reactdv <jerllylara12@gmail.com>
2022-06-21 06:26:21 +0800

    gallery

--- src/container/Gallery/Gallery.js
+++ src/container/Gallery/Gallery.js
@@ -1,11 +1,70 @@
 import React from 'react';
-
+import { useRef } from "react"
 import './Gallery.css';
+import { images } from "../../constants/index.js"
+import  { SubHeading }   from "../../components"
+ import { BsInstagram,BsArrowLeftShort,BsArrowRightShort } from "react-icons/bs"
 
-const Gallery = () => (
-  <div>
-    Gallery
+const gallery = [
+  
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04
+  
+  ]
+
+
+
+const Gallery = () => {
+  
+  const scrollRef = useRef(null)
+  
+  const scroll =(direction)=>{
+  
+    const { current } = scrollRef
+  
+    if(direction==="left"){
+      current.scrollLeft -= 300
+    }else{
+      current.scrollLeft += 300
+    }
+  }
+  
+  
+  
+  return(
+  <div className="app__gallery flex__center" >
+ 
+    <div className="app__gallery-content" >
+     
+      <SubHeading title="Instagram"/>
+      <h1 className="headtext__cormorant">Photo gallery</h1>
+      <p className="p__opensans" style={{color:"#aaa" ,marginTop:"2rem"}}>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <button type="button" className="custom__button">view more
+      </button>
+    </div>
+    <div className="app__gallery-images">
+       <div className="app__gallery-images_container" ref={scrollRef}>
+          {gallery.map((image,index)=>(
+            
+            <div key={index} className="app__gallery-images_card flex__center ">
+               <img src={image}/>
+               <BsInstagram className="gallery__image-icon"/>
+            </div>
+            
+            ))}
+       </div>
+       <div className="app__gallery-images_arrows">
+         <BsArrowLeftShort className="gallery__arrow-icon" onClick={()=>{scroll("left")}}/>
+         <BsArrowRightShort className="gallery__arrow-icon" onClick={()=>{scroll("right")}}/>
+         
+       </div>
+    </div>
+    
   </div>
 );
-
+}
 export default Gallery;
