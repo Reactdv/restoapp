bb39b4b5e0a92eaf362e1e970834a75e662f03b4
Reactdv <jerllylara12@gmail.com>
2022-06-20 08:23:10 +0800

    chef

--- src/container/Chef/Chef.js
+++ src/container/Chef/Chef.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
 import './Chef.css';
+import { images } from "../../constants/images"
+import { SubHeading } from "../../components"
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding ">
+   
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef"/>
+    </div> 
+    
+    <div className="app__wrapper_info">
+       <SubHeading title="Chef's word"/>
+       <h1 className="headtext__cormorant">What we believe in </h1>
+       <div className="app__chef-content">
+          <div className="app__chef-content_qoute">
+             <img src={images.quote}alt="quote"/>
+             <p className="p__opensans">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+             </p>
+          </div>
+          <p className="p__opensans">
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </p>
+       </div>
+       <div className="app__chef-sign">
+          <p>Kevin Lou</p>
+          <p className="p__opensans">
+          Chef & Founder </p>
+          <img src={images.sign} alt="sign"/>
+       </div>
+    </div>
+    
   </div>
+
 );
 
 export default Chef;
