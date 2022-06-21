984f80d1a697e5cda52ebcb6eb90d5271b2cacd8
Reactdv <jerllylara12@gmail.com>
2022-06-21 16:11:07 +0800

    done

--- src/components/Footer/NewsLetter.js
+++ src/components/Footer/NewsLetter.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 import './NewsLetter.css';
-
-const Newsletter = () => (
-  <div>
-    Newsletter
+import  SubHeading  from "../SubHeading/SubHeading.js"
+ 
+ 
+ const Newsletter = () => (
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading"> 
+       <SubHeading title="Newsletter"/>
+       <h1 className="headtext__cormorant">
+          Subscribe To Our Newsletter
+       </h1>
+       <p className="p__opensans">
+          And never miss latest updates
+       </p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+       <input 
+       type="email"
+       placeholder="Enter your email address"
+       />
+       <button className="custom__button">
+         Subscribe
+       </button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -1,10 +1,77 @@
 import React from 'react';
-
+import { FooterOverlay,NewsLetter } from "../../components"
+import { images } from "../../constants"
 import './Footer.css';
+import { FiFacebook,FiTwitter,FiInstagram} from "react-icons/fi"
+
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+  
+   <FooterOverlay />
+   <NewsLetter />
+   <div className="app__footer-links"> 
+     
+     <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext"> contact us
+        </h1>
+        <p className="p__opensans">
+          9W 53rd St New York, 10019 USA
+        </p>
+        
+        <p className="p__opensans">
+         +1 212-344-1230
+        </p>
+        
+        <p className="p__opensans">
+         +1 212-555-1320
+        </p>
+        
+     </div>
+    
+     <div className="app__footer-links_logo">
+       <img src={images.gericht}/>
+       <p className="p__opensans">
+        the best way to find yourself is to lose yourself in the service of others
+       </p>
+       <img 
+       style={{marginTop:"15px"}}
+       className="spoon__img "
+       src={images.spoon}/>
+       <div className="app__footer-links_icons">
+        <FiFacebook />
+        <FiTwitter />
+        <FiInstagram />
+       </div>
+     </div>
+    
+     <div className="app__footer-links_work">
+     
+         <h1 className="app__footer-headtext"> working hours
+         </h1>
+         <p className="p__opensans">
+           Monday-Friday
+         </p>
+        
+         <p className="p__opensans">
+           8:00 am - 12:00 pm
+         </p>
+        
+         <p className="p__opensans">
+         Saturday - Sunday
+         </p>
+     
+         <p className="p__opensans">
+         10:00 am - 1:00 pm 
+         </p>
+     
+     </div>
+ 
+   </div> 
+   <div className="app__footer-links_copyright">
+      <p className="p__opensans">
+      2022 Gericht. All rights reserved</p>
+   </div>
   </div>
 );
 
